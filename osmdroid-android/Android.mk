1cb206ecf3217c6888a28d1cae00a7b87a42e318
mar-v-in <github@rvin.mooo.com>
2014-03-25 17:42:52 +0100

    Fix Android.mk

--- osmdroid-android/Android.mk
+++ osmdroid-android/Android.mk
@@ -1,8 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := osmdroid
-LOCAL_STATIC_JAVA_LIBRARIES := slf4j-api
+LOCAL_MODULE := osmdroid-android
+LOCAL_STATIC_JAVA_LIBRARIES := slf4j-android
 LOCAL_SRC_FILES := $(call all-java-files-under, src/main/java)
 LOCAL_SRC_FILES := $(filter-out src/main/java/org/osmdroid/contributor/GpxToPHPUploader.java,$(LOCAL_SRC_FILES))
 
